d796f7128d37da0f3fc9f9131303a91172bec85f
박준규 <wnsrb003@naver.com>
2022-04-12 23:18:35 +0900

    어제 못한 문제 제출

--- wnsrb003/2번째 제출/17.py
+++ wnsrb003/2번째 제출/17.py
@@ -5,11 +5,27 @@ import sys
 n = int(sys.stdin.readline().strip())
 탑 = list(map(int, sys.stdin.readline().strip().split()))
 결과 = [0]*n
-while len(탑) >1:
-    뺀놈 = 탑.pop()
-    for i in range(len(탑)-1, 0, -1):
-        if 탑[i] >= 뺀놈:
-            결과[len(탑)] = i+1
+
+스택 = []
+for k in range(len(탑)):
+    if not 스택 :
+        스택.append((k, 탑[k]))
+        continue
+    while len(스택):
+        뺄놈 = 스택[-1]
+        if 뺄놈[1] > 탑[k]:
+            결과[k] = 뺄놈[0] + 1
+            스택.append((k, 탑[k]))
             break
+        elif 뺄놈[1] == 탑[k]:
+            결과[k] = 뺄놈[0] + 1
+            스택.pop()
+            스택.append((k, 탑[k]))
+            break
+        else :
+            스택.pop()
+            
+    if not 스택:
+        스택.append((k, 탑[k]))
 
 print(' '.join(list(map(str, 결과))))
